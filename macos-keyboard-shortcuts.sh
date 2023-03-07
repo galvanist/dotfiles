c79eb1153f3250e973785602ce38cdef083843d6
Gavin Leake <gleake@gmail.com>
2023-03-07 13:06:28 +0000

    try a method to apply keyboard shortcuts without reboot

--- macos-keyboard-shortcuts.sh
+++ macos-keyboard-shortcuts.sh
@@ -8,3 +8,9 @@
 # /usr/libexec/PlistBuddy -c "Set :AppleSymbolicHotKeys:27:enabled bool true" ~/Library/Preferences/com.apple.symbolichotkeys.plist
 /usr/libexec/PlistBuddy -c "Delete :AppleSymbolicHotKeys:98:enabled" ~/Library/Preferences/com.apple.symbolichotkeys.plist
 /usr/libexec/PlistBuddy -c "Add :AppleSymbolicHotKeys:98:enabled bool true" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+
+# Ask the system to read the hotkey plist file and ignore the output. Likely updates an in-memory cache with the new plist values.
+defaults read com.apple.symbolichotkeys.plist > /dev/null
+
+# Run reactivateSettings to apply the updated settings.
+/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
